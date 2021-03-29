3ff166425a7e18148cb3590257d4524c575af574
OkIGotIt <okigotit2018@gmail.com>
2021-03-29 01:41:03 -0600

    Add project files.

---
+++ Fresh_Bison_Steak/Fresh_Bison_Steak.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using System.IO;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace Fresh_Bison_Steak
+{
+    [BepInPlugin("com.OkIGotIt.Fresh_Bison_Steak", "Fresh_Bison_Steak", "1.0.0")]
+    public class Fresh_Bison_Steak : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.GlobalEventManager.OnCharacterDeath += GlobalEventManager_OnCharacterDeath;
+        }
+
+        private void GlobalEventManager_OnCharacterDeath(On.RoR2.GlobalEventManager.orig_OnCharacterDeath orig, GlobalEventManager self, DamageReport damageReport)
+        {
+            orig(self, damageReport);
+            CharacterBody attacker = damageReport.attackerBody;
+            int itemCount = attacker.inventory.GetItemCount(ItemCatalog.FindItemIndex("FlatHealth"));
+            if (itemCount > 0)
+                attacker.AddTimedBuff(RoR2Content.Buffs.MeatRegenBoost, 3f * (float)itemCount);
+        }
+    }
+}
