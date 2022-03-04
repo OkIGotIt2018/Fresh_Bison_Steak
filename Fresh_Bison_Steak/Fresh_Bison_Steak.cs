b59211b95c2075d8b87123ea2a6fad0bb3ab8271
OkIGotIt2018 <OkIGotIt2018@gmail.com>
2022-03-04 12:53:36 -0700

    1.0.3

--- Fresh_Bison_Steak/Fresh_Bison_Steak.cs
+++ Fresh_Bison_Steak/Fresh_Bison_Steak.cs
@@ -8,26 +8,21 @@ using System.Security.Permissions;
 
 namespace Fresh_Bison_Steak
 {
-    [BepInPlugin("com.OkIGotIt.Fresh_Bison_Steak", "Fresh_Bison_Steak", "1.0.2")]
+    [BepInPlugin("com.OkIGotIt.Fresh_Bison_Steak", "Fresh_Bison_Steak", "1.0.3")]
     public class Fresh_Bison_Steak : BaseUnityPlugin
     {
         public void Awake()
         {
-            On.RoR2.GlobalEventManager.OnCharacterDeath += GlobalEventManager_OnCharacterDeath;
+            GlobalEventManager.onCharacterDeathGlobal += GlobalEventManager_onCharacterDeathGlobal;
         }
 
-        private void GlobalEventManager_OnCharacterDeath(On.RoR2.GlobalEventManager.orig_OnCharacterDeath orig, GlobalEventManager self, DamageReport damageReport)
+        private void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
         {
-            orig(self, damageReport);
-            if (!damageReport.attacker || !damageReport.attackerBody)
+            if (!report.attacker || !report.attackerBody)
                 return;
-            CharacterBody body = LocalUserManager.GetFirstLocalUser().cachedMasterController.master.GetBody();
-            CharacterBody attacker = damageReport.attackerBody;
-            if (body != attacker)
-                return;
-            int itemCount = attacker.inventory.GetItemCount(ItemCatalog.FindItemIndex("FlatHealth"));
+            int itemCount = report.attackerBody.inventory.GetItemCount(ItemCatalog.FindItemIndex("FlatHealth"));
             if (itemCount > 0)
-                attacker.AddTimedBuff(RoR2Content.Buffs.MeatRegenBoost, 3f * (float)itemCount);
+                report.attackerBody.AddTimedBuff(RoR2Content.Buffs.MeatRegenBoost, 3f * (float)itemCount);
         }
     }
 }
